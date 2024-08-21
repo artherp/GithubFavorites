bbdc75fa350fb1eab5d39ee49aac6a5090f4cb4d
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-21 14:44:18 -0300

    Métodos estáticos e promessas

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint)
+        .then(data => data.json())
+        .then(({ login, name, public_repos, followers }) => ({
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+    }
+}
+
 //classe que vai conter a logica dos dados 
 //como os dados serão estruturados
 export class Favorites {
